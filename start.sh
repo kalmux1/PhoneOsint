89a95633c2795df102aba0bebdf41bcf428b4890
kalmux1 <kal.mux.cyber@gmail.com>
2024-01-05 23:25:00 +0530

    Assets

--- start.sh
+++ start.sh
@@ -14,7 +14,7 @@ sleep 3s
 read -p "Enter a phone number :- " num
 mkdir Assets
 
-truecallerpy -e -n -s $num > /Assets/log.txt
+truecallerpy -e -n -s $num > Assets/log.txt
 
-grep -e "id" -e "name" -e "gender" -e "image" -e "access" -e "Formate" -e "number" -e "Code" -e "carrier" -e "type" -e "address" -e "city" -e "timeZone" -e "service" -e "caption" -e "num" -e "spam" -e "num" -e "top" /Assets/log.txt > /Assets/fil.txt
+grep -e "id" -e "name" -e "gender" -e "image" -e "access" -e "Formate" -e "number" -e "Code" -e "carrier" -e "type" -e "address" -e "city" -e "timeZone" -e "service" -e "caption" -e "num" -e "spam" -e "num" -e "top" /Assets/log.txt > Assets/fil.txt
 
