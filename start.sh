159e625422818c944f6f0b87854351aac4298cf2
kalmux1 <kal.mux.cyber@gmail.com>
2024-01-05 23:05:56 +0530

    asset check

--- start.sh
+++ start.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+# This is a bash Script To provide information of a phone number 
+
+
+echo " PhoneOsint"
+
+
+echo "Installing Required packages"
+echo "This might can take some time based on your internet speed"
+src=$(pip install truecallerpy)
+sleep 3s
+
+read -p "Enter a phone number" num
+
+truecallerpy -e -n -s $num > /Assets/log.txt
+
+grep -e "id" -e "name" -e "gender" -e "image" -e "access" -e "Formate" -e "number" -e "Code" -e "carrier" -e "type" -e "address" -e "city" -e "timeZone" -e "service" -e "caption" -e "num" -e "spam" -e "num" -e "top" /Assets/log.txt > /Assets/fil.txt
+
