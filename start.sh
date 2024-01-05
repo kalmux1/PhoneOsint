b51f354c21dd162014598f38fa2d4718c3a19c19
kalmux1 <kal.mux.cyber@gmail.com>
2024-01-05 23:31:17 +0530

    Assets

--- start.sh
+++ start.sh
@@ -18,3 +18,8 @@ truecallerpy -e -n -s $num > Assets/log.txt
 
 grep -e "id" -e "name" -e "gender" -e "image" -e "access" -e "Formate" -e "number" -e "Code" -e "carrier" -e "type" -e "address" -e "city" -e "timeZone" -e "service" -e "caption" -e "num" -e "spam" -e "num" -e "top" Assets/log.txt > Assets/fil.txt
 
+sed 's/[^[:alnum:][:space:]]//g' Assets/fil.txt > Assets/sym.txt
+
+awk '{print $1,"   :   ",$2}' Assets/sym.txt > Assets/num.txt
+
+head -n -3 Assets/num.txt
