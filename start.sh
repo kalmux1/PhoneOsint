13f8c0459e7d36d8d57f6e868888e7748c659e9a
kalmux1 <kal.mux.cyber@gmail.com>
2024-01-06 12:31:57 +0530

    Final Look

--- start.sh
+++ start.sh
@@ -17,46 +17,7 @@ Purple='\033[0;35m'       # Purple
 Cyan='\033[0;36m'         # Cyan
 White='\033[0;37m'        # White
 Orange='\033[0;33m'       # Orange
-
-# Bold
-BBlack='\033[1;30m'       # Black
-BRed='\033[1;31m'         # Red
-BGreen='\033[1;32m'       # Green
-BYellow='\033[1;33m'      # Yellow
-BBlue='\033[1;34m'        # Blue
-BPurple='\033[1;35m'      # Purple
-BCyan='\033[1;36m'        # Cyan
-BWhite='\033[1;37m'       # White
-
-# Underline
-UBlack='\033[4;30m'       # Black
-URed='\033[4;31m'         # Red
-UGreen='\033[4;32m'       # Green
-UYellow='\033[4;33m'      # Yellow
-UBlue='\033[4;34m'        # Blue
-UPurple='\033[4;35m'      # Purple
-UCyan='\033[4;36m'        # Cyan
-UWhite='\033[4;37m'       # White
-
-# Background
-On_Black='\033[40m'       # Black
-On_Red='\033[41m'         # Red
-On_Green='\033[42m'       # Green
-On_Yellow='\033[43m'      # Yellow
-On_Blue='\033[44m'        # Blue
-On_Purple='\033[45m'      # Purple
-On_Cyan='\033[46m'        # Cyan
-On_White='\033[47m'       # Whit
-
-# High Intensity
-IBlack='\033[0;90m'       # Black
-IRed='\033[0;91m'         # Red
-IGreen='\033[0;92m'       # Green
-IYellow='\033[0;93m'      # Yellow
-IBlue='\033[0;94m'        # Blue
-IPurple='\033[0;95m'      # Purple
-ICyan='\033[0;96m'        # Cyan
-IWhite='\033[0;97m'       # White
+Dark_Gray='\033[1;30'     # Dark Gray
 
 # Bold High Intensity
 BIBlack='\033[1;90m'      # Black
@@ -68,19 +29,7 @@ BIPurple='\033[1;95m'     # Purple
 BICyan='\033[1;96m'       # Cyan
 BIWhite='\033[1;97m'      # White
 
-# High Intensity backgrounds
-On_IBlack='\033[0;100m'   # Black
-On_IRed='\033[0;101m'     # Red
-On_IGreen='\033[0;102m'   # Green
-On_IYellow='\033[0;103m'  # Yellow
-On_IBlue='\033[0;104m'    # Blue
-On_IPurple='\033[0;105m'  # Purple
-On_ICyan='\033[0;106m'    # Cyan
-On_IWhite='\033[0;107m'   # White
-
-
-
-
+# Banner
 
 echo -e "$BIRed 
 
@@ -98,9 +47,11 @@ echo -e " $BICyan             _  _   _     ___ _  _     _
 
 echo -e " $BIYellow                         Installing Required packages
              This might can take some time based on your internet speed $Normal"
-src=$(pip install truecallerpy && sudo apt-get install wget && sudo apt-get install curl )
+src=$(pip install truecallerpy && sudo apt-get install wget )
 echo "
 "
+
+
 sleep 3s
 path=$(pwd)
 read -e -p $'\033[1;95m Enter a phone number :- \033[0m' num
@@ -130,3 +81,4 @@ echo -e "$Orange"
 wget -O image.png --no-verbose --show-progress --quiet $image
 echo -e "$Normal"
 
+echo -e "$Dark_Gray You Can Check Image in the PhoneOsint Directory $Normal"
