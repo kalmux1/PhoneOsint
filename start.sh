ca6b53dd4b8bae5365cdc62534e3e5e25518cb73
kalmux1 <kal.mux.cyber@gmail.com>
2024-01-06 22:52:44 +0530

    v1.0

--- start.sh
+++ start.sh
@@ -131,9 +131,10 @@ else
         echo -e "$BIBlue You Can Check Image in the PhoneOsint Directory $Normal"
 
     fi
+fi
+
         echo " "
         echo " "
         echo -e "$BIYellow Visit to my github for more hacking tools https://github.com/kalmux1"
         echo -e "                  Thanks for using this tool                        "
         echo -e "                         Hack daily                                $Normal"
-fi
