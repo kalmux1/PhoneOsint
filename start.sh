f93733a65943ea0f387c74c2c94d1f5ad02bd316
kalmux1 <kal.mux.cyber@gmail.com>
2024-01-06 22:39:04 +0530

    v1.0

--- start.sh
+++ start.sh
@@ -49,23 +49,33 @@ echo -e " $BIYellow                         Installing Required packages
              This might can take some time based on your internet speed $Normal"
 
 
+# "Checking and Installing Required Dependencies"
+
+
+# Checking for pip
 pip_chk=$(which pip)
 if [[ $? != 0 ]]
 then
-    pip=$(sudo apt-get install pip -y)
+    # Insatlling pip if not installed
+    pip=$(sudo apt-get install pip -y)   
 fi
-
+# Checking for Trucallerpy
 src_chk=$(which truecallerpy)
 if [[ $? != 0 ]]
 then
+    # Insatlling Truecallerpy if not installed
     src=$(sudo pip install truecallerpy)
 fi
-
+# Checking for wget
 wget_chk=$(which wget)
 if [[ $? != 0 ]]
 then
+    # Insatlling wget if not installed
     dnl=$(sudo apt-get install wget)
 fi
+
+
+# Checking For Login ID
 
 login=$(truecallerpy --installationid)
 if [[ $? != 0 ]]
@@ -75,40 +85,55 @@ else
     echo "
     "
     path=$(pwd)
+    # Taking Phone Number as Input From User
     read -e -p $'\033[1;95m Enter a phone number :- \033[0m' num
-
+    # Checking Length of Number
     len=${#num}
     if [[ $len -gt 10 ]]
     then 
+        # If Number is Invalid Then exiting
         echo " "
         echo -e "$BIRed         Invalid Number $Normal "
     else
         if [[ ! -d $path/Assets ]]
         then 
+            # Making directory for storing tools files and logs
             mkdir $path/Assets
         fi
+        # Gathering Information of the number via Trucallerpy and storing it in log.txt
         truecallerpy -e -n -s $num > Assets/log.txt
-
+        # Extracting Requiered output and storing it in fil.txt
         grep -w -e "id" -e "name" -e "gender" -e "access" -e "e164Formate" -e "numberType" -e "nationalFormat" -e "dialingCode" -e "carrier" -e "type" -e "spamScore" -e "spamType" -e "address" -e "city" -e "countryCode" -e "timeZone" -e "service" -e "caption" -e "numReports" -e "numReports60days" -e"numSearches60days" -e "numCalls60days" -e "numMessages60days" -e "type" Assets/log.txt > Assets/fil.txt
-
+        # Removing Unessasary Special Characters From The extracted file and storing it in sym.txt
         sed 's/[^[:alnum:][:space:]]//g' Assets/fil.txt > Assets/sym.txt
-
+        # Formating final data of the number and storing it in num.txt
         awk '{print "    ",$1," ===> ",$2}' Assets/sym.txt > Assets/num.txt
+        # Printing final information 
         echo -e "$BIGreen"
         head -n -2 Assets/num.txt
         echo -e "$Normal"
+        # Extracting image link data from the log.txt
         grep -e "image" Assets/log.txt > Assets/img.txt || exit 1
-
+        # Storing main link into image.txt
         cmd=$(awk '{print $2}' Assets/img.txt > Assets/image.txt)
+        # Removing "" from the link
         image=$(tr '" ,' ' ' < Assets/image.txt )
-
         sleep 2s
         echo " "
+        # Downloading the image
         echo -e "$BIBlue Downloading The Profile Image $Normal"
         echo -e "$Orange"
+        # Storing image in image.png 
         wget -O image.png --no-verbose --show-progress --quiet $image
         echo -e "$Normal"
-
+        # image msg
         echo -e "$BIBlue You Can Check Image in the PhoneOsint Directory $Normal"
+
+        echo " "
+        echo " "
+        echo -e "$BIYellow Visit to my github for more hacking tools https://github.com/kalmux1"
+        echo -e "                  Thanks for using this tool                        "
+        echo -e "                         Hack daily                                $Normal"
+
     fi
 fi
